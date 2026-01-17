3f2190421caffe5f88806648befc4bea9ffc0f7e
Felix <154911483+zfelyx@users.noreply.github.com>
2026-01-18 00:00:53 +0100

    refactor: Remove ToolsPage component and its routing logic from main application

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import ToolsPage from './components/ToolsPage'
 import NotFound from './components/NotFound'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 const pathname = typeof window !== 'undefined' ? window.location.pathname : '/'
 
-if (pathname.startsWith('/tools')) {
-    root.render(
-        <React.StrictMode>
-            <ToolsPage />
-        </React.StrictMode>,
-    )
-} else if (pathname === '/' || pathname === '/index.html') {
+if (pathname === '/' || pathname === '/index.html') {
     root.render(
         <React.StrictMode>
             <App />
