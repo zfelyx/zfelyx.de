909758506b5c796ee7a44c17d06ef61bb971b9c7
Felix <154911483+zfelyx@users.noreply.github.com>
2026-01-17 14:25:31 +0100

    feat: Update TypeScript configuration to include vite-env.d.ts for environment variables

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly VITE_EMAILJS_SERVICE_ID: string
+  readonly VITE_EMAILJS_TEMPLATE_ID: string
+  readonly VITE_EMAILJS_PUBLIC_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
