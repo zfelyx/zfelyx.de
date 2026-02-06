27350d864805ecacd1b09d09b22c700fa6d448b6
Felix <154911483+zfelyx@users.noreply.github.com>
2026-02-06 10:29:06 +0100

    Change Email Adresse

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
                 <div className="flex space-x-4 sm:space-x-6 mb-6">
                   <Social title="GitHub" href='https://github.com/zfelyx' path='github.svg' color="#24292e"/>
                   <Social title="Discord" href='https://discord.com/users/1159547960233836684' path='discord.svg' color="#5865F2"/>
-                  <Social title="Email" href='mailto:felix.schneider008@outlook.de' path='envelope.svg' color="#D44638"/>
+                  <Social title="Email" href='mailto:contact@zfelyx.de' path='envelope.svg' color="#D44638"/>
                 </div>
               </div>
               <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
